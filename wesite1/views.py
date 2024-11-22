b6a942f9f0b579fcf2c4fb86057e52d0adf4043a
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2024-11-22 14:00:16 +0330

    SET index.html with static files (css,img,js)

--- wesite1/views.py
+++ wesite1/views.py
@@ -4,11 +4,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return render(request, 'index.html')
+    return render(request, 'wesite1/index.html')
 
 def about_view(request):
-    return render(request, 'about.html')
+    return render(request, 'wesite1/about.html')
 
 def contact_view(request):
-    return render(request, 'contact.html')
+    return render(request, 'wesite1/contact.html')
     #return HttpResponse('<h1>contact Page</h1>')
