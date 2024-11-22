8ee50f8fa5734cee7d07456bf49a812cc4828f68
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2024-11-22 12:19:59 +0330

    static files (linked test.css)

--- wesite1/views.py
+++ wesite1/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index_view(request):
-    return render(request, 'web/index.html')
+    return render(request, 'index.html')
 
 def about_view(request):
     return render(request, 'about.html')
