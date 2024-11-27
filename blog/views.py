c10b7a6c61ae8fe6c8f3d5925ff74da0e67ed5cf
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2024-11-27 21:06:40 +0330

    06.08

--- blog/views.py
+++ blog/views.py
@@ -13,10 +13,25 @@ def blog_view(request):
     return render(request, 'blog/blog-single.html', context=context)"""
 
 def blog_single(request, post_id):
-    posts = get_object_or_404(Post,status=1, id = post_id)    
-    posts.counted_views += 1
-    posts.save()
-    context = {'posts': posts}
-    return render(request, 'blog/blog-single.html', context=context)
+    current_post = get_object_or_404(Post,status=1, id = post_id)    
+    current_post.counted_views += 1
+    current_post.save()
+    
+    
+    
+    #context = {'post': current_post}
+    #return render(request, 'blog/blog-single.html', context=context)
 
+
+    all_posts = Post.objects.all().order_by('published_date')    
+    current_index = list(all_posts).index(current_post)
+    previous_post = all_posts[current_index - 1] if current_index > 0 else None
+    next_post = all_posts[current_index + 1] if current_index < len(all_posts) - 1 else None
+    context = {
+        'current_post': current_post,
+        'previous_post': previous_post,
+        'next_post': next_post,
+    }
+    
+    return render(request, 'blog/blog-single.html', context)
 
