6bfbe10c79be1d11296e7c1de457b06c868d64de
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2024-12-03 18:24:56 +0330

    0912 comment system creation

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post,Category
+from blog.models import Post,Category,Comment
 # Register your models here.
 
 from django_summernote.admin import SummernoteModelAdmin
@@ -17,5 +17,14 @@ class PostAdmin (SummernoteModelAdmin):
 
     summernote_fields = ('content',)
  
+
+class CommentAdmin (admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = "-empty-"
+    #fields = [ "title"]
+    #exclude = ["birth_date"]
+    list_display = ["name","post", "approved","created_date",'updated_date']
+    list_filter =  ["post",'updated_date',"approved",]
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
+admin.site.register(Comment, CommentAdmin)
