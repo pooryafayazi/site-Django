e89a221dad8c3cff0a9fccf9882758beb851a456
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2024-12-04 17:04:57 +0330

    10.03 login form in template

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 # Create your views here.
 def login_view(request):
-    if request.user.is_authenticated:
-        msg = f"user is authenticated as {request.user.username}"
-    else:
-        msg = f"user is not authenticated"
-    return render(request, 'accounts/login.html', {'msg': msg})
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            #msg = f"user is authenticated as {request.user.username}"
+            return redirect('/')
+        #else:
+            #msg = f"user is not authenticated"
+    return render(request, 'accounts/login.html')
 
 #def logout_view(request):
     
